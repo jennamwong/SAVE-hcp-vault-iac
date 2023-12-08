2fed418fa07b80ebb3af26004b4da9210188d532
Jenna Wong <jenna.wong@hashicorp.com>
2023-12-08 12:44:56 -0600

    Adding Vault config

--- main.tf
+++ main.tf
@@ -6,4 +6,21 @@ terraform {
       name = "hcp-vault-iac"
     }
   }
+
+  required_providers {
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "~> 0.77.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "hcp" {}
+
+provider "aws" {
+  region = var.region
 }
