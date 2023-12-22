531dc1caca4296b297a2ada4301e7bb378ee7b53
Syed Quadri <syed.quadri@hashicorp.com>
2023-12-21 18:56:36 -0600

    auth

--- auth.tf
+++ auth.tf
@@ -1,34 +1,16 @@
-# resource "vault_auth_backend" "github" {
-#   type = "github"
-# }
+resource "vault_auth_backend" "userpass" {
+  type = "userpass"
+}
 
-# resource "vault_auth_backend" "ldap" {
-#   type = "ldap"
-# }
+resource "vault_generic_endpoint" "u1" {
+  depends_on           = [vault_auth_backend.userpass]
+  path                 = "auth/userpass/users/u1"
+  ignore_absent_fields = true
 
-# resource "vault_auth_backend" "okta" {
-#   type = "okta"
-# }
-
-# resource "vault_github_auth_backend" "github" {
-#   organization = "IAC-Project-SAVE"
-# }
-#---------changes----------
-
-# resource "vault_auth_backend" "userpass" {
-#   type = "userpass"
-# }
-
-# resource "vault_generic_endpoint" "test" {
-#   depends_on           = [vault_auth_backend.userpass]
-#   path                 = "auth/userpass/users/test"
-#   ignore_absent_fields = true
-#   # namespace = "admin/value-engineering/academy"
-
-#   data_json = <<EOT
-# {
-#   "policies": ["user-policy"],
-#   "password": "admin"
-# }
-# EOT
-# }
+  data_json = <<EOT
+{
+  "policies": ["user-policy"],
+  "password": "changeme"
+}
+EOT
+}
