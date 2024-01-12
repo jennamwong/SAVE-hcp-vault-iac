72dfae2d623547074ff19a06ee754d6cbbdb5e49
Syed Quadri <syed.quadri@hashicorp.com>
2024-01-11 20:09:33 -0600

    admin

--- auth.tf
+++ auth.tf
@@ -6,11 +6,10 @@ resource "vault_generic_endpoint" "u1" {
   depends_on           = [vault_auth_backend.userpass]
   path                 = "auth/userpass/users/admin"
   ignore_absent_fields = true
-  namespace = "VE"
 
   data_json = <<EOT
 {
-  "policies": ["VE-policy"],
+  "policies": ["super-admin-policy"],
   "password": "admin"
 }
 EOT
