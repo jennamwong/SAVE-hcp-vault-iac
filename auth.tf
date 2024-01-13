c904dfe2a2c104ff3f9f238ba9cedd02be9e22e7
Syed Quadri <syed.quadri@hashicorp.com>
2024-01-12 18:38:12 -0600

    admin

--- auth.tf
+++ auth.tf
@@ -2,6 +2,11 @@ resource "vault_auth_backend" "userpass" {
   type = "userpass"
 }
 
+resource "vault_auth_backend" "userpass_ve" {
+  type = "userpass"
+  namespace = "VE"
+}
+
 resource "vault_generic_endpoint" "u1" {
   depends_on           = [vault_auth_backend.userpass]
   path                 = "auth/userpass/users/admin"
@@ -17,7 +22,7 @@ EOT
 
 resource "vault_generic_endpoint" "u2" {
   depends_on           = [vault_auth_backend.userpass]
-  path                 = "auth/userpass/users/veuser"
+  path                 = "VE/auth/userpass/users/veuser"
   ignore_absent_fields = true
 
   data_json = <<EOT
