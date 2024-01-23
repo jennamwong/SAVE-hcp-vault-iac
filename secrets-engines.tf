78ede553b4d51660d096144bc49332446febd0a3
Syed Quadri <syed.quadri@hashicorp.com>
2024-01-23 13:33:43 -0600

    admin

---
+++ secrets-engines.tf
@@ -0,0 +1,31 @@
+resource "vault_mount" "kvv2-example" {
+  path        = "version2-example"
+  namespace = "admin"
+  type        = "kv-v2"
+  options = {
+    version = "2"
+    type    = "kv-v2"
+  }
+  description = "This is an example KV Version 2 secret engine mount"
+}
+
+resource "vault_mount" "transit-example" {
+  path        = "transit-example"
+  namespace = "VE"
+  type        = "transit"
+  description = "This is an example transit secret engine mount"
+
+  options = {
+    convergent_encryption = false
+  }
+}
+
+resource "vault_mount" "pki-example" {
+  path        = "pki-example"
+  namespace = "CDL"
+  type        = "pki"
+  description = "This is an example PKI mount"
+
+  default_lease_ttl_seconds = 3600
+  max_lease_ttl_seconds     = 86400
+}
