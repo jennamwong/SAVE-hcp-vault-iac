da2912c9de644bfb7f23cdc12a1b5d638c4ec8ae
Syed Quadri <syed.quadri@hashicorp.com>
2024-01-23 09:50:31 -0600

    admin

--- auth.tf
+++ auth.tf
@@ -24,7 +24,7 @@ resource "vault_generic_endpoint" "u1" {
 
   data_json = <<EOT
 {
-  "policies": ["super_admin"],
+  "policies": ["super-admin"],
   "password": "test"
 }
 EOT
--- policies.tf
+++ policies.tf
@@ -1,9 +1,16 @@
-resource "vault_policy" "super_admin" {
-     name   = "super_admin"
-     policy = file("policies/super_admin.hcl")
+resource "vault_policy" "super-admin" {
+     name   = "super-admin"
+     policy = file("policies/super-admin.hcl")
  }
 
-# resource "vault_policy" "veuser" {
-#     name   = "VE-policy"
-#     policy = file("policies/VE-policy.hcl")
-# }
+resource "vault_policy" "ve-senior" {
+    name   = "ve-senior"
+    namespace = "VE"
+    policy = file("policies/VE-policy-senior.hcl")
+}
+
+resource "vault_policy" "ve-junior" {
+    name   = "ve-junior"
+    namespace = "VE"
+    policy = file("policies/VE-policy-junior.hcl")
+}
