889af042061b1228f1c2cb81664bbffbc3378ace
Syed Quadri <syed.quadri@hashicorp.com>
2024-01-23 10:56:12 -0600

    admin

--- policies.tf
+++ policies.tf
@@ -20,3 +20,11 @@ resource "vault_policy" "VE-junior" {
     namespace = "VE"
     policy = file("policies/VE-policy-junior.hcl")
 }
+
+resource "vault_egp_policy" "preventkvv1" {
+  name              = "prevent-kv-v1-engines"
+  paths             = ["admin"]
+  enforcement_level = "hard-mandatory"
+
+  policy = "prevent-kv-v1-engines" 
+}
