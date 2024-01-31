5e188bb87a2348cc351bb3c253960b5edd45ca0c
Syed Quadri <syed.quadri@hashicorp.com>
2024-01-31 16:31:33 -0600

    admin

--- auth.tf
+++ auth.tf
@@ -108,13 +108,13 @@ resource "vault_approle_auth_backend_role" "jenkins" {
   token_policies  = ["jenkins"]
 }
 
-resource "vault_approle_auth_backend_role_secret_id" "id" {
-  backend   = vault_auth_backend.approle.path
-  role_name = vault_approle_auth_backend_role.jenkins.role_name
-}
+# resource "vault_approle_auth_backend_role_secret_id" "id" {
+#   backend   = vault_auth_backend.approle.path
+#   role_name = vault_approle_auth_backend_role.jenkins.role_name
+# }
 
-resource "vault_approle_auth_backend_login" "login" {
-  backend   = vault_auth_backend.approle.path
-  role_id   = vault_approle_auth_backend_role.jenkins.role_id
-  secret_id = vault_approle_auth_backend_role_secret_id.id.secret_id
-}
+# resource "vault_approle_auth_backend_login" "login" {
+#   backend   = vault_auth_backend.approle.path
+#   role_id   = vault_approle_auth_backend_role.jenkins.role_id
+#   secret_id = vault_approle_auth_backend_role_secret_id.id.secret_id
+# }
